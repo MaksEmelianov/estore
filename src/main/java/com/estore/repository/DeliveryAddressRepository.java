6efd115bb426d9517b2887dba9bc6ae719650c2c
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2023-02-03 20:04:20 +0500

    Added repositories

---
+++ src/main/java/com/estore/repository/DeliveryAddressRepository.java
@@ -0,0 +1,11 @@
+package com.estore.repository;
+
+import com.estore.entity.DeliveryAddress;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeliveryAddressRepository extends CrudRepository<DeliveryAddress, Long> {
+}
---
+++ src/main/java/com/estore/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.estore.repository;
+
+import com.estore.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+}
